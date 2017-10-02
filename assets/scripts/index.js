72cf9b085ce61e762c6bb05206557e274129f123
hadleyana <hadleysweck@gmail.com>
2017-10-02 15:12:12 -0400

    Basic Styles and Html
    added
    -tabs
    -pages
    -sign in/up forms
    -some basic logic for switching tabs

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,11 +2,28 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const views = require('./views')
 
 $(() => {
   setAPIOrigin(location, config)
 })
 
+$(() => {
+  $('#landingPageBtn').click(function () {
+    views.openTab(event, 'landingPage')
+  })
+  $('#aboutPageBtn').click(function () {
+    views.openTab(event, 'aboutPage')
+  })
+  $('#chartPageBtn').click(function () {
+    views.openTab(event, 'chartPage')
+  })
+  $('#myPatternsPageBtn').click(function () {
+    views.openTab(event, 'myPatternsPage')
+  })
+})
+
+// onclick="openTab(event, 'landingPage')"
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
